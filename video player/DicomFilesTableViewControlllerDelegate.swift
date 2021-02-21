3b22efc3afe588c8b6982a65a6248170b459dddd
Ed Cafferatra <cafferata@live.nl>
2021-02-21 12:57:05 +0100

    spaces

--- video player/DicomFilesTableViewControlllerDelegate.swift
+++ video player/DicomFilesTableViewControlllerDelegate.swift
@@ -1,14 +1,12 @@
 //
 //  DicomFilesTableViewControlllerDelegate.swift
 //  dicom player
-//
 //  Created by Ed Cafferata on 31/01/2021.
 //  Copyright © 2021 Cafferata. All rights reserved.
 //
 import Foundation
 ///
 /// Delegate protocol for the view controller that displays the list of files (DicomFilesTableViewController).
-///
 /// Used to inform the main ViewController that user wants to load a Dicom File on it.
 ///
 protocol DicomFilesTableViewControllerDelegate: class {
